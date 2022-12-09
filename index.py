69cebbfb6dd9b23f3a834993563bc4dee566256f
Kilian <67644676+ichunichu@users.noreply.github.com>
2022-12-09 08:43:23 +0100

    fetch video

--- index.py
+++ index.py
@@ -28,14 +28,19 @@ from appwrite.services.storage import Storage
 client = Client()
 
 (client
-  .set_endpoint('http://45.140.164.127/v1') # Your API Endpoint
+  .set_endpoint('https://appwrite.senditeverywhere.com/v1') # Your API Endpoint
   .set_project('638e3cb0dea2968f1afa') # Your project ID
-  .set_key('84cc4810c6b3e545190b46c5d1c58aab51cf497be5c71d386de82db6a5bfdafb0638cbde819eb58bd532a0570b45e61b6b168265b1a79fbecc236c589b83b078017d056731ca584055eb923108b5c5999efa332c09457e2c9df6b425a9f5514ac7aab1b7f73556211d9bbc4f8d1b24c03707c0adce1ffbab8d686eb29a1c01e4') # Your secret API key
+  .set_key('5a0f11cb675717984e00cda60f975ea1f24bb37a0b3831bb19af973a5947305645789398ffc3fee76aa19ec28086d890e2527dd67aea252859b7feda1e318016f2da15bd63fc4219b584f1598d2c9cb2d40e1e1fa98a1c42397d5dbbf554303ac3f8acffcb526daddaf721418ee217b4bbc7e341fd418f4f7b026b93bd54fc26') # Your secret API key
 )
 
 storage = Storage(client)
 
-result = storage.get_file_download('638e3cbb9709b0fdce1e', '638e5c2d93101fa2f253')
+with open("test.mp4",mode="rb") as file:
+    result = file.read()
 
 print(result.__sizeof__())
+
+#result = storage.get_file_download('638efd926527d6cbf083', '638eff2461938913eae7')
+
+
 
