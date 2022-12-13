2694a4319b1a3e8c10f9a9a8970ddf31f278351d
Kilian <67644676+ichunichu@users.noreply.github.com>
2022-12-13 15:30:44 +0100

    working upload

--- gpt.py
+++ gpt.py
@@ -1,36 +1,21 @@
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from google_auth_oauthlib.flow import Flow
 
-#flow = Flow.from_client_secrets_file(
-#    'bureau.json',
-#    scopes=['https://www.googleapis.com/auth/youtube.upload'],
-#    redirect_uri='urn:ietf:wg:oauth:2.0:oob')
-#code = '4/0AfgeXvu75aw5U_1mXW3RBgxd-Ix6HGjOO26BIeAXLGpMbif2XuMI9QbKtqpMQT4NywZJqQ'
-#flow.fetch_token(code=code)
-#
-## Create a credentials object by passing the API key to the `Credentials` class
-#creds = flow.credentials
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+client_id = os.environ.get("client_id")
+client_secret = os.environ.get("client_secret")
 
-#{
-#	"access_token": "ya29.a0AeTM1icZ5iiF2pwE-uBhFeK63qXfVXwoY8D-gg_og1LBV11JY9ia9geDcubSWePVZxr8rzt48yigkdpd5Jt0PgjWFf5a48h2iYeM52_6nEAKbt7YxNjPHYUJXUtDzGbAX2n2p-XdgySgrY5bWGzcW87eIIi6aCgYKAVASARASFQHWtWOmnU9tQWnuA3fIIhiSFD63uA0163",
-#	"expires_in": 3599,
-#	"refresh_token": "1//09eSzaNO7YAnkCgYIARAAGAkSNwF-L9IrK5qGD5_ONPO4jq5EhQPuL7fxt60sknpKyklPbCezjX08RbR5Vi957xlnO_K6qy9hS7Q",
-#	"scope": "https://www.googleapis.com/auth/youtube.upload",
-#	"token_type": "Bearer"
-#}
 
 creds = Credentials(
-            token="ya29.a0AeTM1icZ5iiF2pwE-uBhFeK63qXfVXwoY8D-gg_og1LBV11JY9ia9geDcubSWePVZxr8rzt48yigkdpd5Jt0PgjWFf5a48h2iYeM52_6nEAKbt7YxNjPHYUJXUtDzGbAX2n2p-XdgySgrY5bWGzcW87eIIi6aCgYKAVASARASFQHWtWOmnU9tQWnuA3fIIhiSFD63uA0163",
-            refresh_token="1//09eSzaNO7YAnkCgYIARAAGAkSNwF-L9IrK5qGD5_ONPO4jq5EhQPuL7fxt60sknpKyklPbCezjX08RbR5Vi957xlnO_K6qy9hS7Q",
-            #id_token=token_dict.get('_id_token'),
-
-    token_uri= "https://accounts.google.com/o/oauth2/token"
-,
-client_id="785621539450-af6ja0ahpmuj7te7a4t3o5pbquehhmtj.apps.googleusercontent.com",
-            client_secret="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")
+            token="ya29.a0AeTM1ie3Fm3LZnjlE7FExPz6_lB-BmjRgUXHTkBSw94RLPqE-2Y058vmMKMjZ7iqvSBw6FzGhBVXs0XH9Gg2DhJeuP6HIAjvZBNtjabUKQ94MLjUyYNvGnzH3qMOq1JJdrVCYuV32GePZj4OpY8p0Xt9yr46JAaCgYKAcISARASFQHWtWOmcyNpirisCQYT2Ny44CU02A0165",
+            refresh_token="1//0371hKAgOHRM4CgYIARAAGAMSNwF-L9IrsXomcTeLJJrOqTRNXf5_AM6qM9v3BiDxDK3vrMv0NUoTeokvm6GDXq2MMfNf9NwcmSU",
+            token_uri= "https://accounts.google.com/o/oauth2/token",
+            client_id=client_id,
+            client_secret=client_secret)
 
 # Build the YouTube API client
 youtube = build("youtube", "v3", credentials=creds)
@@ -54,5 +39,3 @@ with open("test.mp4", "rb") as file:
 
         media_body=media
     ).execute()
-#Quotat du jour atteint. La ligne media_body doit etre tester (je l'avais retirer pour essayer de debug le code, mais
-# ducoup le quotat as était atteint avant que je pense a la remettre)
--- index.py
+++ index.py
@@ -1,93 +1,71 @@
+import json
 import threading
 
+from appwrite.client import Client
+from appwrite.id import ID
+from appwrite.services.storage import Storage
+from appwrite.services.databases import Databases
+from appwrite.query import Query
+
+client = Client()
+database = Databases(client)
+(client
+.set_endpoint('https://appwrite.senditeverywhere.com/v1')  # Your API Endpoint
+.set_project('638e3cb0dea2968f1afa')  # Your project ID
+.set_key(
+    '5a0f11cb675717984e00cda60f975ea1f24bb37a0b3831bb19af973a5947305645789398ffc3fee76aa19ec28086d890e2527dd67aea252859b7feda1e318016f2da15bd63fc4219b584f1598d2c9cb2d40e1e1fa98a1c42397d5dbbf554303ac3f8acffcb526daddaf721418ee217b4bbc7e341fd418f4f7b026b93bd54fc26')
+)
+
 from flask import Flask, request, Response
+from flask_cors import CORS, cross_origin
 from datetime import timedelta, datetime
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client_id = os.environ.get("client_id")
+client_secret = os.environ.get("client_secret")
+
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+
 
 def doStuff(*args):
     print("I DID STUFF")
     print(args)
 
+
+
+
+
 @app.route('/webhook', methods=['POST'])
 def respond():
-    res = request.json;
+    res = request.json
     if res["$collectionId"] == "638e47c1f24248bbcda6":
         now = datetime.now()
         print(type(request.json["vidId"]));
         run_at = now + timedelta(seconds=3)
         delay = (run_at - now).total_seconds()
-        threading.Timer(delay, doStuff, [{"vidId":res["vidId"]}]).start()
+        threading.Timer(delay, doStuff, [{"vidId": res["vidId"]}]).start()
     return Response(status=200)
 
-#app.run()
 
-from appwrite.client import Client
-from appwrite.services.storage import Storage
-from appwrite.services.databases import Databases
-from appwrite.query import Query
+# app.run()
 
-from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
-from googleapiclient.http import MediaFileUpload
-
-
-client = Client()
-database = Databases(client)
-(client
-  .set_endpoint('https://appwrite.senditeverywhere.com/v1') # Your API Endpoint
-  .set_project('638e3cb0dea2968f1afa') # Your project ID
-  .set_key('5a0f11cb675717984e00cda60f975ea1f24bb37a0b3831bb19af973a5947305645789398ffc3fee76aa19ec28086d890e2527dd67aea252859b7feda1e318016f2da15bd63fc4219b584f1598d2c9cb2d40e1e1fa98a1c42397d5dbbf554303ac3f8acffcb526daddaf721418ee217b4bbc7e341fd418f4f7b026b93bd54fc26') # Your secret API key
-)
 
 storage = Storage(client)
 
+# print(result.__sizeof__())
 
-
-#print(result.__sizeof__())
-
-#get all schedules
-toUpload = database.list_documents("63910075cc7582964627","6391a27459ba27f94278",[Query.equal("uploaded", False), Query.orderAsc(attribute="dateTime")])
+# get all schedules
+toUpload = database.list_documents("63910075cc7582964627", "6391a27459ba27f94278",
+                                   [Query.equal("uploaded", False), Query.orderAsc(attribute="dateTime")])
 
 if toUpload["documents"]:
     print(toUpload["documents"])
-
-
-
-credentials = Credentials("ya29.a0AeTM1icCU5P3PhlbCjlUFBqn6n0R5--0CXn9zh7xo5-0-wS4G94cjJrxhs_Uey6o6DhD9HHul_DnJN-lsi1DAmL9ArAZvnxe15VdJtFIa4L5sOAEc390sI_9zzhNtxyGUgGHbq0I2vgDcpyglcbjUxf9aWwLaCgYKAUESARASFQHWtWOmTQtPuMKMwsOonQb7sDX30g0163")
-
-client = build('youtube', 'v3', credentials)
-
-body = {
-            'snippet': {
-                'title': 'My Django Youtube Video',
-                'description': 'My Django Youtube Video Description',
-                'tags': 'django,howto,video,api',
-                'categoryId': '27'
-            },
-            'status': {
-                'privacyStatus': 'unlisted'
-            }
-        }
-#https://developers.google.com/identity/protocols/oauth2/web-server#httprest_3
-with open("test.mp4",mode="rb") as file:
-    result = file.read()
-    insert_request = client.videos().insert(
-                part=','.join(body.keys()),
-                body=body,
-                media_body=MediaFileUpload(
-                    file.name, chunksize=-1, resumable=True)
-            )
-    insert_request.execute()
-
-
-
-
-
-
-
-
-#result = storage.get_file_download('638efd926527d6cbf083', '638eff2461938913eae7')
-
-
 
